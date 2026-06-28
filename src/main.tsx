641f5ebf163daa09018651681296327c7e729c52
Habib332 <ha8795691@gmail.com>
2026-06-28 16:30:26 +0500

    added dark mode and fixed the search inbox page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ChatProvider } from './context/ChatContext'
-import { ThemeProvider } from './hooks/ThemeContext'
+import { ThemeProvider } from './hooks/ThemeContext.tsx'
 import './hooks/Theme.css'   // ← global tokens, loaded before anything renders
 import './index.css'
 import App from './App.tsx'
